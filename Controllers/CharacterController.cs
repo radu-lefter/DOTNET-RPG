77a91b5553ab44e0dd87896620ae20ce0cafcde9
raduubraduu <lefter_radu@yahoo.com>
2023-04-03 21:44:15 +0100

    created dtos to get and ad characters

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -18,19 +18,19 @@ namespace dotnet_rpg.Controllers
         }
 
         [HttpGet("GetAll")]
-        public async Task<ActionResult<List<Character>>> Get()
+        public async Task<ActionResult<List<GetCharacterDto>>> Get()
         {
             return Ok(await _characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
-         public async Task<ActionResult<Character>> GetSingle(int id)
+         public async Task<ActionResult<GetCharacterDto>> GetSingle(int id)
         {
             return Ok(await _characterService.GetCharacterById(id));
         }
 
         [HttpPost]
-        public async Task<ActionResult<List<Character>>> AddCharacter(Character newCharacter)
+        public async Task<ActionResult<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
         {
             return Ok(await _characterService.AddCharacter(newCharacter));
         }
