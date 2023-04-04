460d4a173297bb972194895c49351b5f527a660e
raduubraduu <lefter_radu@yahoo.com>
2023-04-04 14:45:29 +0100

    updated delete method on controller

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -86,13 +86,16 @@ namespace dotnet_rpg.Services.CharacterService
 
             try
             {
-                var character = characters.FirstOrDefault(c => c.Id == id);
+                var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
                 if (character is null)
                     throw new Exception($"Character with Id '{id}' not found.");
 
-                characters.Remove(character);
+                _context.Characters.Remove(character);
 
-                serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+                await _context.SaveChangesAsync();
+
+                serviceResponse.Data = 
+                    await _context.Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToListAsync();
             }
             catch (Exception ex)
             {
