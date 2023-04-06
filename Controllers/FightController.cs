ced036ef9d51eafe0926dd3ac9f1f182bc314512
raduubraduu <lefter_radu@yahoo.com>
2023-04-06 21:58:39 +0100

    implemented fight service and controller

--- Controllers/FightController.cs
+++ Controllers/FightController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using dotnet_rpg.Dtos.Fight;
 
 namespace dotnet_rpg.Controllers
 {
@@ -14,6 +15,12 @@ namespace dotnet_rpg.Controllers
         public FightController(IFightService fightService)
         {
             _fightService = fightService;
+        }
+
+        [HttpPost("Weapon")]
+        public async Task<ActionResult<ServiceResponse<AttackResultDto>>> WeaponAttack(WeaponAttackDto request)
+        {
+            return Ok(await _fightService.WeaponAttack(request));
         }
     }
 }
